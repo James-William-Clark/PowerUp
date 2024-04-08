a84e167799300eafb175195ec1e28111abd07de4
James-William-Clark <jamesclark199804@gmail.com>
2024-04-08 21:27:58 +1000

    Split out home screen and profile into their own files

--- PowerUpExpoApp/App.js
+++ PowerUpExpoApp/App.js
@@ -1,49 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import * as React from 'react';
-//import {HomeScreen, ProfileScreen} from './src/screens/Home.js'
+import HomeScreen from './src/screens/HomeScreen.js'
+import ProfileScreen from './src/screens/ProfileScreen.js'
+
 
 const Stack = createNativeStackNavigator();
-function  HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        title="My profile"
-        onPress={() =>
-          navigation.navigate('Profile')
-        }
-      />
-      <Button
-        title="Workout Routines"
-        onPress={() =>
-          navigation.navigate('Profile')
-        }
-      />
-      <Button
-        title="Start a new workout"
-        onPress={() =>
-          navigation.navigate('Profile')
-        }
-      />
-      <Button
-        title="Past Workouts"
-        onPress={() =>
-          navigation.navigate('Profile')
-        }
-      />
-    </View>
-  );
-};
-
-function ProfileScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>My Profile</Text>
-    </View>
-  );
-}
 
 export default function App() {
   return (
