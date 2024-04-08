582388d8459ba070af2cc3c76329a10630462dbe
James-William-Clark <jamesclark199804@gmail.com>
2024-04-08 20:26:07 +1000

    Added buttons to homescreen

--- PowerUpExpoApp/App.js
+++ PowerUpExpoApp/App.js
@@ -1,12 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import * as React from 'react';
+//import {HomeScreen, ProfileScreen} from './src/screens/Home.js'
+
+const Stack = createNativeStackNavigator();
+function  HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="My profile"
+        onPress={() =>
+          navigation.navigate('Profile')
+        }
+      />
+      <Button
+        title="Workout Routines"
+        onPress={() =>
+          navigation.navigate('Profile')
+        }
+      />
+      <Button
+        title="Start a new workout"
+        onPress={() =>
+          navigation.navigate('Profile')
+        }
+      />
+      <Button
+        title="Past Workouts"
+        onPress={() =>
+          navigation.navigate('Profile')
+        }
+      />
+    </View>
+  );
+};
+
+function ProfileScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>My Profile</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
