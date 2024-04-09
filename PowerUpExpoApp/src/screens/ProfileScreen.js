97f8afb0947213864139a86b35712d46e26e4514
James-William-Clark <jamesclark199804@gmail.com>
2024-04-09 20:27:03 +1000

    Added basic options for user profile input

--- PowerUpExpoApp/src/screens/ProfileScreen.js
+++ PowerUpExpoApp/src/screens/ProfileScreen.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 
 export default function ProfileScreen({ navigation }) {
-    return (
+  return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>My Profile</Text>
+        <TextInput placeholder='Name'/>
+        <TextInput placeholder='Weight'/>
+        <TextInput placeholder='Height'/>
       </View>
     );
   }
+
   
