0eed72c89d01262d5a75db1f5f3b5bb701563eac
James-William-Clark <jamesclark199804@gmail.com>
2024-05-11 20:01:25 +1000

    Implemented workout record to view control

--- PowerUpExpoApp/src/Views/Screens/History/PastWorkoutModalScreenViewControl.tsx
+++ PowerUpExpoApp/src/Views/Screens/History/PastWorkoutModalScreenViewControl.tsx
@@ -1,25 +1,32 @@
 
 import * as React from 'react';
+import { ExerciseRecord } from '../../../Core/ExerciseRecord';
 
 export default class PastWorkoutModalScreenViewModel {
+    // TODO: Getters/Setters
     Name: string;
     SetName: any;
     TimeTaken: string;
     SetTime: any;
-    ExerciseList: Array<Exercise>;
+    ExerciseList: ExerciseRecord[];
+    SetExercises: any;
 
     constructor() {
         [this.Name, this.SetName] = React.useState('');
         [this.TimeTaken, this.SetTime] = React.useState('');
-        [this.ExerciseList, this.SetExercises] = React.useState('');
+        [this.ExerciseList, this.SetExercises] = React.useState([]);
     
     }
     async LoadPastWorkout(workoutId : number) : Promise<void> {
         // placeholder until saving workouts is implemented
         if (workoutId == 1) {
-            return <Text>Workout 1 Info</Text>;
+            this.SetName("Strength Training - Beginner");
+            this.SetTime("1:10")
+            this.SetExercises([new ExerciseRecord("Bench Press", "3x5", "50KG")]);
         } else if (workoutId == 2) {
-            return <Text>Workout 2 Info</Text>
+            this.SetName("Couch to 10K");
+            this.SetTime("0:31")
+            this.SetExercises([new ExerciseRecord("Running", "31 minutes", "9km/hr")])
         }
     }
 
