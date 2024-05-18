e1b37f4bd538b72fd3a5f4bdb69732b91773e604
James-William-Clark <jamesclark199804@gmail.com>
2024-05-18 22:47:07 +1000

    Created exercise class

---
+++ PowerUpExpoApp/src/Core/Exercise.tsx
@@ -0,0 +1,11 @@
+export class Exercise {
+    Name : string;
+    Description: string;
+    Category: string;
+
+    constructor(name : string, description : string, category : string) {
+        this.Name = name;
+        this.Description = description;
+        this.Category = category;
+    }
+}
