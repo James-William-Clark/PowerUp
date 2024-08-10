3403e990362c40fc357e9e822e2d8f50da8255a7
James-William-Clark <jamesclark199804@gmail.com>
2024-08-10 17:12:31 +1000

    Added add exercise to navigator

--- PowerUpExpoApp/src/Views/Screens/Workout/WorkoutHomeStackNavigator.tsx
+++ PowerUpExpoApp/src/Views/Screens/Workout/WorkoutHomeStackNavigator.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WorkoutLoggerScreenView from "./WorkoutLoggerScreenView";
 import WorkoutHomeScreenView from "./WorkoutHomeScreenView";
+import AddExerciseView from "./AddExerciseView";
 
 export default function WorkoutHomeStackNavigator() {
 
@@ -10,7 +11,7 @@ export default function WorkoutHomeStackNavigator() {
         <Stack.Navigator screenOptions={{headerShown:false}}>
           <Stack.Screen name="WorkoutHomeScreen" component={WorkoutHomeScreenView} />
           <Stack.Screen name="WorkoutLoggerScreen" component={WorkoutLoggerScreenView} />
-
+          <Stack.Screen name="AddExerciseView" component={AddExerciseView} />
         </Stack.Navigator>
   
       );
