777d1581b3395c9ce054c88bd6fd95d6d171ebae
James-William-Clark <jamesclark199804@gmail.com>
2024-08-10 17:12:18 +1000

    Added add exercise screen

---
+++ PowerUpExpoApp/src/Views/Screens/Workout/AddExerciseView.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { View, StyleSheet, ScrollView, Text, Button } from 'react-native';
+
+export default function AddExerciseView({route, navigation} : any) {
+    const exercises = [
+        {name : "Squat"}
+    ]
+    return (
+        <View>
+            <ScrollView>
+                {exercises.map((exercise, index) => (
+                    <View key={index}>
+                        <Text style={styles.row}>{exercise.name}</Text>
+                        <Button onPress = {() => {navigation.navigate("WorkoutLoggerScreen", { exercise : exercise })}} title="Add"/>
+                  </View>
+                ))}
+            </ScrollView>
+            <Button title="Cancel" onPress={() => navigation.goBack()} color="#FF0000"/>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+      top:0,
+      flex: 1,
+      backgroundColor: '#FFFFFF',
+    },
+    row: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderBottomWidth: 1,
+      borderBottomColor: '#E0E0E0',
+    },
+    icon: {
+      marginRight: 10,
+    },
+    workoutName: {
+      flex: 1,
+      fontWeight: 'bold',
+      fontSize: 16,
+      color: '#2F2F2F',
+    },
+    xp: {
+      fontSize: 16,
+      color: '#4b0082',
+    },
+  });
