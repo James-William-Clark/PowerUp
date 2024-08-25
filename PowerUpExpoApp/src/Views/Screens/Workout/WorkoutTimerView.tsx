2c87fa42fed435630e9c2dfec95d1e9794e47727
James-William-Clark <jamesclark199804@gmail.com>
2024-08-25 16:23:19 +1000

    Appended 0 to seconds less than 10

--- PowerUpExpoApp/src/Views/Screens/Workout/WorkoutTimerView.tsx
+++ PowerUpExpoApp/src/Views/Screens/Workout/WorkoutTimerView.tsx
@@ -18,7 +18,7 @@ export default function WorkoutTimerView() {
         var hours = Math.floor(time / 3600);
         var minutes = Math.floor(time % 3600 / 60);
         var seconds = Math.floor(time % 3600 % 60);
-        return `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds}`
+        return `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
     }
 
     return (
